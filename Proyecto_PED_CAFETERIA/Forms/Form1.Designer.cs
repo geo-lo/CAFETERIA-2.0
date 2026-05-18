af7788d22b7105d20feba70d89e75ef09c8480b4
Cesar <136837471+CesarSerrano503@users.noreply.github.com>
2026-05-17 22:07:45 -0600

    Mejora diseño del login de administrador

--- Proyecto_PED_CAFETERIA/Forms/Form1.Designer.cs
+++ Proyecto_PED_CAFETERIA/Forms/Form1.Designer.cs
@@ -4,9 +4,15 @@
     {
         private System.ComponentModel.IContainer components = null;
 
-        private System.Windows.Forms.Panel panelLogin;
+        private System.Windows.Forms.Panel panelPrincipal;
+        private System.Windows.Forms.Panel panelIzquierdo;
+        private System.Windows.Forms.Panel panelDerecho;
+        private System.Windows.Forms.Label lblIcono;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblFrase;
         private System.Windows.Forms.Label lblTitulo;
         private System.Windows.Forms.Label lblSubtitulo;
+        private System.Windows.Forms.Panel lineaDecorativa;
         private System.Windows.Forms.Label lblUsuario;
         private System.Windows.Forms.Label lblContrasena;
         private System.Windows.Forms.TextBox txtUsuario;
@@ -14,7 +20,6 @@
         private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Button btnCancelar;
         private System.Windows.Forms.Label lblError;
-        private System.Windows.Forms.Label lblCredenciales;
 
         protected override void Dispose(bool disposing)
         {
@@ -28,181 +33,236 @@
 
         private void InitializeComponent()
         {
-            this.panelLogin = new System.Windows.Forms.Panel();
+            this.panelPrincipal = new System.Windows.Forms.Panel();
+            this.panelIzquierdo = new System.Windows.Forms.Panel();
+            this.lblIcono = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblFrase = new System.Windows.Forms.Label();
+            this.panelDerecho = new System.Windows.Forms.Panel();
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.lineaDecorativa = new System.Windows.Forms.Panel();
             this.lblSubtitulo = new System.Windows.Forms.Label();
             this.lblUsuario = new System.Windows.Forms.Label();
-            this.lblContrasena = new System.Windows.Forms.Label();
             this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lblContrasena = new System.Windows.Forms.Label();
             this.txtContrasena = new System.Windows.Forms.TextBox();
             this.btnIngresar = new System.Windows.Forms.Button();
             this.btnCancelar = new System.Windows.Forms.Button();
             this.lblError = new System.Windows.Forms.Label();
-            this.lblCredenciales = new System.Windows.Forms.Label();
-            this.panelLogin.SuspendLayout();
+            this.panelPrincipal.SuspendLayout();
+            this.panelIzquierdo.SuspendLayout();
+            this.panelDerecho.SuspendLayout();
             this.SuspendLayout();
-
             // 
             // frmLogin
             // 
+            this.AcceptButton = this.btnIngresar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(245, 235, 220);
-            this.ClientSize = new System.Drawing.Size(460, 430);
-            this.Controls.Add(this.panelLogin);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.BackColor = System.Drawing.Color.FromArgb(238, 225, 208);
+            this.CancelButton = this.btnCancelar;
+            this.ClientSize = new System.Drawing.Size(760, 460);
+            this.Controls.Add(this.panelPrincipal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmLogin";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Acceso administrador";
-
             // 
-            // panelLogin
+            // panelPrincipal
             // 
-            this.panelLogin.BackColor = System.Drawing.Color.White;
-            this.panelLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelLogin.Controls.Add(this.lblTitulo);
-            this.panelLogin.Controls.Add(this.lblSubtitulo);
-            this.panelLogin.Controls.Add(this.lblUsuario);
-            this.panelLogin.Controls.Add(this.txtUsuario);
-            this.panelLogin.Controls.Add(this.lblContrasena);
-            this.panelLogin.Controls.Add(this.txtContrasena);
-            this.panelLogin.Controls.Add(this.btnIngresar);
-            this.panelLogin.Controls.Add(this.btnCancelar);
-            this.panelLogin.Controls.Add(this.lblError);
-            this.panelLogin.Controls.Add(this.lblCredenciales);
-            this.panelLogin.Location = new System.Drawing.Point(40, 28);
-            this.panelLogin.Name = "panelLogin";
-            this.panelLogin.Size = new System.Drawing.Size(380, 370);
-            this.panelLogin.TabIndex = 0;
-
+            this.panelPrincipal.BackColor = System.Drawing.Color.White;
+            this.panelPrincipal.Controls.Add(this.panelIzquierdo);
+            this.panelPrincipal.Controls.Add(this.panelDerecho);
+            this.panelPrincipal.Location = new System.Drawing.Point(45, 40);
+            this.panelPrincipal.Name = "panelPrincipal";
+            this.panelPrincipal.Size = new System.Drawing.Size(670, 375);
+            this.panelPrincipal.TabIndex = 0;
+            // 
+            // panelIzquierdo
+            // 
+            this.panelIzquierdo.BackColor = System.Drawing.Color.FromArgb(124, 70, 39);
+            this.panelIzquierdo.Controls.Add(this.lblIcono);
+            this.panelIzquierdo.Controls.Add(this.lblMarca);
+            this.panelIzquierdo.Controls.Add(this.lblFrase);
+            this.panelIzquierdo.Location = new System.Drawing.Point(0, 0);
+            this.panelIzquierdo.Name = "panelIzquierdo";
+            this.panelIzquierdo.Size = new System.Drawing.Size(230, 375);
+            this.panelIzquierdo.TabIndex = 0;
+            // 
+            // lblIcono
+            // 
+            this.lblIcono.Font = new System.Drawing.Font("Segoe UI Emoji", 42F, System.Drawing.FontStyle.Bold);
+            this.lblIcono.ForeColor = System.Drawing.Color.White;
+            this.lblIcono.Location = new System.Drawing.Point(0, 58);
+            this.lblIcono.Name = "lblIcono";
+            this.lblIcono.Size = new System.Drawing.Size(230, 70);
+            this.lblIcono.TabIndex = 0;
+            this.lblIcono.Text = "☕";
+            this.lblIcono.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Bold);
+            this.lblMarca.ForeColor = System.Drawing.Color.White;
+            this.lblMarca.Location = new System.Drawing.Point(20, 140);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(190, 95);
+            this.lblMarca.TabIndex = 1;
+            this.lblMarca.Text = "Dulce\r\nAroma";
+            this.lblMarca.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblFrase
+            // 
+            this.lblFrase.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lblFrase.ForeColor = System.Drawing.Color.FromArgb(250, 232, 210);
+            this.lblFrase.Location = new System.Drawing.Point(28, 265);
+            this.lblFrase.Name = "lblFrase";
+            this.lblFrase.Size = new System.Drawing.Size(175, 50);
+            this.lblFrase.TabIndex = 2;
+            this.lblFrase.Text = "Acceso exclusivo para administración";
+            this.lblFrase.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelDerecho
+            // 
+            this.panelDerecho.BackColor = System.Drawing.Color.White;
+            this.panelDerecho.Controls.Add(this.lblTitulo);
+            this.panelDerecho.Controls.Add(this.lineaDecorativa);
+            this.panelDerecho.Controls.Add(this.lblSubtitulo);
+            this.panelDerecho.Controls.Add(this.lblUsuario);
+            this.panelDerecho.Controls.Add(this.txtUsuario);
+            this.panelDerecho.Controls.Add(this.lblContrasena);
+            this.panelDerecho.Controls.Add(this.txtContrasena);
+            this.panelDerecho.Controls.Add(this.btnIngresar);
+            this.panelDerecho.Controls.Add(this.btnCancelar);
+            this.panelDerecho.Controls.Add(this.lblError);
+            this.panelDerecho.Location = new System.Drawing.Point(230, 0);
+            this.panelDerecho.Name = "panelDerecho";
+            this.panelDerecho.Size = new System.Drawing.Size(440, 375);
+            this.panelDerecho.TabIndex = 1;
             // 
             // lblTitulo
             // 
-            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold);
-            this.lblTitulo.ForeColor = System.Drawing.Color.Sienna;
-            this.lblTitulo.Location = new System.Drawing.Point(20, 25);
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.ForeColor = System.Drawing.Color.FromArgb(94, 52, 30);
+            this.lblTitulo.Location = new System.Drawing.Point(55, 45);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(340, 45);
+            this.lblTitulo.Size = new System.Drawing.Size(330, 50);
             this.lblTitulo.TabIndex = 0;
-            this.lblTitulo.Text = "Modo administrador";
-            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
+            this.lblTitulo.Text = "Administrador";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lineaDecorativa
+            // 
+            this.lineaDecorativa.BackColor = System.Drawing.Color.FromArgb(166, 84, 45);
+            this.lineaDecorativa.Location = new System.Drawing.Point(60, 98);
+            this.lineaDecorativa.Name = "lineaDecorativa";
+            this.lineaDecorativa.Size = new System.Drawing.Size(65, 4);
+            this.lineaDecorativa.TabIndex = 1;
             // 
             // lblSubtitulo
             // 
             this.lblSubtitulo.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.lblSubtitulo.ForeColor = System.Drawing.Color.DimGray;
-            this.lblSubtitulo.Location = new System.Drawing.Point(20, 72);
+            this.lblSubtitulo.Location = new System.Drawing.Point(58, 112);
             this.lblSubtitulo.Name = "lblSubtitulo";
-            this.lblSubtitulo.Size = new System.Drawing.Size(340, 25);
-            this.lblSubtitulo.TabIndex = 1;
-            this.lblSubtitulo.Text = "Ingrese sus credenciales para continuar";
-            this.lblSubtitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
+            this.lblSubtitulo.Size = new System.Drawing.Size(330, 25);
+            this.lblSubtitulo.TabIndex = 2;
+            this.lblSubtitulo.Text = "Inicie sesión para habilitar las opciones del sistema";
+            this.lblSubtitulo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // lblUsuario
             // 
             this.lblUsuario.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            this.lblUsuario.ForeColor = System.Drawing.Color.FromArgb(70, 70, 70);
-            this.lblUsuario.Location = new System.Drawing.Point(45, 120);
+            this.lblUsuario.ForeColor = System.Drawing.Color.FromArgb(55, 55, 55);
+            this.lblUsuario.Location = new System.Drawing.Point(60, 160);
             this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(290, 22);
-            this.lblUsuario.TabIndex = 2;
+            this.lblUsuario.Size = new System.Drawing.Size(310, 22);
+            this.lblUsuario.TabIndex = 3;
             this.lblUsuario.Text = "Usuario";
-
             // 
             // txtUsuario
             // 
+            this.txtUsuario.BackColor = System.Drawing.Color.FromArgb(250, 247, 243);
+            this.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 11F);
-            this.txtUsuario.Location = new System.Drawing.Point(48, 145);
+            this.txtUsuario.Location = new System.Drawing.Point(60, 185);
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(285, 27);
+            this.txtUsuario.Size = new System.Drawing.Size(315, 27);
             this.txtUsuario.TabIndex = 0;
             this.txtUsuario.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUsuario_KeyDown);
-
             // 
             // lblContrasena
             // 
             this.lblContrasena.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
-            this.lblContrasena.ForeColor = System.Drawing.Color.FromArgb(70, 70, 70);
-            this.lblContrasena.Location = new System.Drawing.Point(45, 185);
+            this.lblContrasena.ForeColor = System.Drawing.Color.FromArgb(55, 55, 55);
+            this.lblContrasena.Location = new System.Drawing.Point(60, 225);
             this.lblContrasena.Name = "lblContrasena";
-            this.lblContrasena.Size = new System.Drawing.Size(290, 22);
-            this.lblContrasena.TabIndex = 4;
+            this.lblContrasena.Size = new System.Drawing.Size(310, 22);
+            this.lblContrasena.TabIndex = 5;
             this.lblContrasena.Text = "Contraseña";
-
             // 
             // txtContrasena
             // 
+            this.txtContrasena.BackColor = System.Drawing.Color.FromArgb(250, 247, 243);
+            this.txtContrasena.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtContrasena.Font = new System.Drawing.Font("Segoe UI", 11F);
-            this.txtContrasena.Location = new System.Drawing.Point(48, 210);
+            this.txtContrasena.Location = new System.Drawing.Point(60, 250);
             this.txtContrasena.Name = "txtContrasena";
             this.txtContrasena.PasswordChar = '*';
-            this.txtContrasena.Size = new System.Drawing.Size(285, 27);
+            this.txtContrasena.Size = new System.Drawing.Size(315, 27);
             this.txtContrasena.TabIndex = 1;
             this.txtContrasena.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtContrasena_KeyDown);
-
             // 
             // btnIngresar
             // 
-            this.btnIngresar.BackColor = System.Drawing.Color.Sienna;
+            this.btnIngresar.BackColor = System.Drawing.Color.FromArgb(166, 84, 45);
             this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnIngresar.FlatAppearance.BorderSize = 0;
             this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIngresar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.btnIngresar.ForeColor = System.Drawing.Color.White;
-            this.btnIngresar.Location = new System.Drawing.Point(48, 260);
+            this.btnIngresar.Location = new System.Drawing.Point(60, 305);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(135, 38);
+            this.btnIngresar.Size = new System.Drawing.Size(145, 42);
             this.btnIngresar.TabIndex = 2;
             this.btnIngresar.Text = "Ingresar";
             this.btnIngresar.UseVisualStyleBackColor = false;
             this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
-
             // 
             // btnCancelar
             // 
-            this.btnCancelar.BackColor = System.Drawing.Color.Gray;
+            this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(90, 90, 90);
             this.btnCancelar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCancelar.FlatAppearance.BorderSize = 0;
             this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCancelar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.btnCancelar.ForeColor = System.Drawing.Color.White;
-            this.btnCancelar.Location = new System.Drawing.Point(198, 260);
+            this.btnCancelar.Location = new System.Drawing.Point(230, 305);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(135, 38);
+            this.btnCancelar.Size = new System.Drawing.Size(145, 42);
             this.btnCancelar.TabIndex = 3;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = false;
             this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
-
             // 
             // lblError
             // 
             this.lblError.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.lblError.ForeColor = System.Drawing.Color.DarkRed;
-            this.lblError.Location = new System.Drawing.Point(48, 308);
+            this.lblError.Location = new System.Drawing.Point(60, 350);
             this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(285, 22);
-            this.lblError.TabIndex = 8;
-            this.lblError.Text = "";
+            this.lblError.Size = new System.Drawing.Size(315, 18);
+            this.lblError.TabIndex = 9;
             this.lblError.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
             // 
-            // lblCredenciales
+            // Resume
             // 
-            this.lblCredenciales.Font = new System.Drawing.Font("Segoe UI", 8.5F);
-            this.lblCredenciales.ForeColor = System.Drawing.Color.Gray;
-            this.lblCredenciales.Location = new System.Drawing.Point(48, 333);
-            this.lblCredenciales.Name = "lblCredenciales";
-            this.lblCredenciales.Size = new System.Drawing.Size(285, 22);
-            this.lblCredenciales.TabIndex = 9;
-            this.lblCredenciales.Text = "Admin: admin / 1234";
-            this.lblCredenciales.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
-            this.panelLogin.ResumeLayout(false);
-            this.panelLogin.PerformLayout();
+            this.panelPrincipal.ResumeLayout(false);
+            this.panelIzquierdo.ResumeLayout(false);
+            this.panelDerecho.ResumeLayout(false);
+            this.panelDerecho.PerformLayout();
             this.ResumeLayout(false);
         }
     }
